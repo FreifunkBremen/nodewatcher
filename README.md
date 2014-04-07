eb2e0969827414c1634a960cfe821003eb8d0543
Jan-Philipp Litza <janphilipp@litza.de>
2014-04-07 12:13:48 +0200

    Added basic readme and config sample

---
+++ README.md
@@ -0,0 +1,11 @@
+Nodewatcher
+===========
+
+This software is supposed to watch a network of Freifunk nodes and notify their
+owners if their nodes go down.
+
+It is modularized so that it can take the node status from various sources
+(currently only A.L.F.R.E.D., as used in [gluon]) and notify via various
+channels (currently e-mail and XMPP).
+
+[gluon]: https://github.com/freifunk-gluon/gluon/
---
+++ config.sample.py
@@ -0,0 +1,36 @@
+db = 'sqlite:///nodes.db'
+
+notify_timeout = 60*60 # one hour
+renotify_interval = 24*60*60 # one day
+
+notify_text_long = """Hallo lieber Freifunker!
+
+Dein Knoten %(name)s (MAC-Adresse %(mac)s) ist seit %(since)s offline.
+
+Vielleicht ist er nicht mehr in Reichweite eines benachbarten Knoten oder seine
+VPN-Verbindung ist abgebrochen? Wenn du Probleme hast, den Knoten wieder zum
+Laufen zu bekommen, wende dich gerne an liste@bremen.freifunk.net
+
+Viele Grüße,
+Freifunk Bremen
+
+-- 
+Du erhältst diese Mail, weil die E-Mailadresse %(contact)s als Kontakt bei der
+Einrichtung dieses Knotens angegeben wurde. Du erhältst diese Mail nur einmal
+pro Ausfall des Knotens.
+
+Solltest du für diesen oder alle deine Knoten keine solchen Mails mehr erhalten
+wollen, sende bitte eine Mail an nodewatcher@example.org"""
+
+notify_text_short = "Hey Freifunker, dein Knoten %(name)s (MAC %(mac)s) ist seit %(since)s offline!"
+
+email_from = 'nodewatcher@example.org'
+email_smtp_server = 'mail.example.org'
+email_smtp_username = 'nodewatcher@example.org'
+email_smtp_password = 'secret'
+
+xmpp_server = ('jabber.example.org', 5222)
+xmpp_username = 'nodewatcher@example.org'
+xmpp_password = 'secret'
+
+ca_certs = '/etc/ssl/certs/ca-certificates.crt'
